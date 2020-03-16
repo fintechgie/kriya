58574990304b7af96b8eaf04155c52cd55ea2068
fintechgie <fintechgie@gmail.com>
2020-03-16 16:10:48 +0530

    1

--- lib/Bottom_navbar/History_File.dart
+++ lib/Bottom_navbar/History_File.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 
 class History extends StatefulWidget {
   @override
@@ -100,3 +101,31 @@ class _History extends State<History> {
     );
   }
 }
+
+class ZoomableWidget extends StatefulWidget {
+  final Widget child;
+
+  const ZoomableWidget({Key key, this.child}) : super(key: key);
+  @override
+  _ZoomableWidgetState createState() => _ZoomableWidgetState();
+}
+
+class _ZoomableWidgetState extends State<ZoomableWidget> {
+  Matrix4 matrix = Matrix4.identity();
+
+  @override
+  Widget build(BuildContext context) {
+    return MatrixGestureDetector(
+      onMatrixUpdate: (Matrix4 m, Matrix4 tm, Matrix4 sm, Matrix4 rm) {
+        setState(() {
+          matrix = m;
+        });
+      },
+      child: Transform(
+        transform: matrix,
+        child: widget.child,
+      ),
+    );
+  }
+}
+
